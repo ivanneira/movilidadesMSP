de03452244240db9a150d64cc1a479a04c2bf745
ivanneira <ien1983@gmail.com>
2016-12-29 12:03:23 -0300

    test que devuelve un json

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,26 @@
 <?php
 // Routes
+//
+//$app->get('/[{name}]', function ($request, $response, $args) {
+//    // Sample log message
+//    //$this->logger->info("Slim-Skeleton '/' route");
+//
+//    // Render index view
+//    return $this->renderer->render($response, 'index.phtml', $args);
+//});
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+class claseDeEjemplo {
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    public $var1 = "test";
+
+    public $var2 = "test1";
+}
+
+
+$app->get('/test', function ($request, $response, $args) {
+
+    $testData = new claseDeEjemplo();
+
+    return json_encode($testData);
+
 });
--- src/settings.php
+++ src/settings.php
@@ -4,6 +4,14 @@ return [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
 
+        //configuración de la base de datos
+        'db' => [
+            'host' => "localhost",
+            'user' => "root",
+            'pass' => "",
+            'dbname' => "test"
+        ],
+
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
